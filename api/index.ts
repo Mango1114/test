e6f90a86b0c9392eedb02ed451187cc1aed4fbf4
hulala <969677795@qq.com>
2024-05-17 15:43:22 +0800

    修改

--- api/index.ts
+++ api/index.ts
@@ -3,31 +3,31 @@ const express = require('express');
 const multer = require('multer');
 const app = express();
 
-const upload = multer({ dest: 'uploads/',limits: { fileSize: 10 * 1024 * 1024 },}); 
+const upload = multer({ dest: 'uploads/',limits: { fileSize: 10 * 1024 * 1024 },});
 
 //添加body内容解析器
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.post("/*", (req, res) => {
-    
-//     var value = "Basic " + Buffer.from("weihu" + ":" + "123456").toString('base64');
+app.post("/*", (req, res) => {
 
-//     req.headers['Authorization']=value;
-//     delete req.headers['content-length'];
-//     const data = req.body;
+    var value = "Basic " + Buffer.from("weihu" + ":" + "123456").toString('base64');
 
-//     fetch('http://api.metavans.net:6090'+req.url,{
-//         method: 'POST',
-//         headers:req.headers,
-//         body:data
-//     })
-//     .then(response => response.json())
-//     .then(json => {
-//        // console.log(json);
-//         res.send(json);
-//     }).catch(error => console.error('Error:', error));
-// });
+    req.headers['Authorization']=value;
+    delete req.headers['content-length'];
+    const data = req.body;
+
+    fetch('http://api.metavans.net:6090'+req.url,{
+        method: 'POST',
+        headers:req.headers,
+        body:data
+    })
+    .then(response => response.json())
+    .then(json => {
+       // console.log(json);
+        res.send(json);
+    }).catch(error => console.error('Error:', error));
+});
 
 
 app.post('/upload', upload.single('file'), (req, res) => {
